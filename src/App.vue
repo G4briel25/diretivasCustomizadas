a41bc632fd65a4806415b6740c58bba28c4e3755
G4briel25 <warmerishoth@gmail.com>
2024-08-30 17:53:43 -0400

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
+
 const data = { cor: 'blue', tamanhoFonte: '32px', totalCaracteres: 4 };
+
+const color = {
+  mounted(el) {
+    console.log(el);
+  },
+}
+
 </script>
 
 <template>
@@ -7,11 +15,12 @@ const data = { cor: 'blue', tamanhoFonte: '32px', totalCaracteres: 4 };
     <p v-texto="{ cor: 'red', tamanhoFonte: '200%', totalCaracteres: 15 }">Primeira diretiva customizada</p>
     <p v-texto="data">Teste</p>
     <div v-posicao:absolute="150" v-texto="{cor: 'green', tamanhoFonte: '35px'}">Olá, eu sou um binding de argumento e valor</div>
-  
-    
-    
-  <span>Código A: </span><input type="text"><span v-informacao:simples.umClickMouse.sairAutomaticamente="'Você encontrará o código A na parte frontal do equipamento.'"> i</span>
-  <br>
-  <span>Código B: </span><input type="text"><span v-informacao:destacado.doisClicksMouse.sairAutomaticamente="'Você encontrará o código B na parte transeira do equipamento, abaixo da tampa de proteção.'"> i</span>
+
+
+
+    <span>Código A: </span><input type="text"><span v-informacao:simples.umClickMouse.sairAutomaticamente="'Você encontrará o código A na parte frontal do equipamento.'"> i</span>
+    <br>
+    <span>Código B: </span><input type="text"><span v-informacao:destacado.doisClicksMouse.sairAutomaticamente="'Você encontrará o código B na parte transeira do equipamento, abaixo da tampa de proteção.'"> i</span>
+    <h1 v-color>Diretiva customizada registrada localmente (no componente)</h1>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,100 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import informacao from './directives/informacao';
+import posicao from './directives/posicao';
 
 const app = createApp(App);
 
 app.directive('texto', );
 
-app.directive('posicao', {
-    created(el, binding) {
-        console.log(el, binding.arg, binding.value);
-        
-        const posicoesPossiveis = ['relative', 'fixed', 'absolute'];
+app.directive('posicao', posicao);
 
-        if(posicoesPossiveis.includes(binding.arg)) {
-            el.style.position = binding.arg;
-            el.style.top = `${binding.value}px`;
-        }
-
-    }
-});
-
-app.directive('informacao', {
-    created(el, binding) {
-        console.log(el, binding.arg, binding.modifiers, binding.value)
-        let funcao = null
-
-        if(binding.arg == undefined || binding.arg === 'simples') {
-            funcao = function() {
-                let informacaoSpan = document.createElement('span')
-                informacaoSpan.style.position = 'absolute'
-                informacaoSpan.style.background = 'red'
-                informacaoSpan.style.padding = '2px'
-                informacaoSpan.innerText = binding.value
-
-                el.appendChild(informacaoSpan)
-
-                informacaoSpan.addEventListener('click', (event) => {
-                    event.stopPropagation()
-                    informacaoSpan.remove()
-                })
-
-                if(binding.modifiers['sairAutomaticamente']) {
-                    setTimeout(() => {
-                        informacaoSpan.remove()
-                    }, 3000)
-                }
-            }
-        }
-
-        if(binding.arg === 'destacado') {
-            funcao = function() {
-                let informacaoDivContainer = document.createElement('div')
-                let informacaoDiv = document.createElement('div')
-                informacaoDiv.innerText = binding.value
-
-                informacaoDivContainer.style.with = '100%'
-                informacaoDivContainer.style.height = '100%'
-                informacaoDivContainer.style.display = 'flex'
-                informacaoDivContainer.style.flexDirection = 'row'
-                informacaoDivContainer.style.alignItems = 'center'
-                informacaoDivContainer.style.justifyContent = 'center'
-                informacaoDivContainer.style.background = 'orange'
-                informacaoDivContainer.style.position = 'absolute'
-                informacaoDivContainer.style.top = '0'
-                informacaoDivContainer.style.zIndex = '10'
-                informacaoDivContainer.style.color = 'black'
-
-                informacaoDiv.style.padding = '30px'
-                informacaoDiv.style.background = '#fba'
-
-                informacaoDivContainer.appendChild(informacaoDiv)
-
-                el.appendChild(informacaoDivContainer)
-
-                informacaoDivContainer.addEventListener('click', (event) => {
-                    event.stopPropagation()
-                    informacaoDivContainer.remove()
-                })
-
-                if(binding.modifiers['sairAutomaticamente']) {
-                    setTimeout(() => {
-                        informacaoDivContainer.remove()
-                    }, 3000)
-                }
-            }
-        }
-
-
-        if(binding.modifiers['umClickMouse']) {
-            el.addEventListener('click', funcao)
-        }
-
-        if(binding.modifiers['doisClicksMouse']) {
-            el.addEventListener('dblclick', funcao)
-        }
-    }
-});
+app.directive('informacao', informacao);
 
 app.mount('#app');
